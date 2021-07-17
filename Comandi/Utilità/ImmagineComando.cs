7a594b34a8eb2db1ed7ccb72f4a8a493d1df212d
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-17 20:06:26 +0200

    Create ImmagineComando.cs

---
+++ Comandi/Utilità/ImmagineComando.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace KheetoNetworkBot.Comandi.Moderazione
+{
+    public class ImmagineComando : BaseCommandModule
+    {
+        [Command("immagine")]
+        [Description("Manda un immagine in un embed.")]
+        public async Task Comando(CommandContext command, [Description("Link dell'immagine da convertire.")] string LinkImmagine)
+        {
+
+            if (LinkImmagine.Length == 0)
+            {
+                await command.RespondAsync("Manda il link di un immagine.");
+                return;
+            };
+
+            DiscordEmbedBuilder.EmbedThumbnail thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
+            {
+                Url = LinkImmagine,
+            };
+
+            DiscordEmbedBuilder.EmbedFooter footer = new DiscordEmbedBuilder.EmbedFooter
+            {
+                Text = command.Member.DisplayName,
+                IconUrl = command.Message.Author.AvatarUrl
+            };
+
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder()
+            {
+                Footer = footer,
+                Thumbnail = thumbnail
+            };
+
+            await command.RespondAsync(embed);
+        }
+    }
+}
