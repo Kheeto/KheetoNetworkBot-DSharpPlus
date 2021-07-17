5276b0e8ef4e551eeb542391a9b18bd6a1f63e3b
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-17 18:56:49 +0200

    Create KickComando.cs

---
+++ Comandi/Moderazione/KickComando.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
+
+namespace KheetoNetworkBot.Comandi.Moderazione
+{
+    public class KickComando : BaseCommandModule
+    {
+        [Command("kick")]
+        [Description("Espelle un utente dal server.")]
+        [RequirePermissions(DSharpPlus.Permissions.KickMembers)]
+        public async Task Comando(CommandContext command, [Description("Utente da Espellere.")] DiscordMember Utente, [Description("Motivo dell'Espulsione")] params string[] Motivo)
+        {
+            string motivoFinale = null;
+            if (Motivo.Length > 0)
+            {
+                foreach (string arg in Motivo)
+                {
+                    motivoFinale = motivoFinale + arg + " ";
+                }
+            }
+            try
+            {
+                await Utente.RemoveAsync(motivoFinale);
+                await command.RespondAsync("Utente espulso con successo");
+            }
+            catch (NotFoundException)
+            {
+                await command.RespondAsync("Utente non trovato - DSharpPlus.Exceptions.NotFoundException");
+            }
+            catch (BadRequestException)
+            {
+                await command.RespondAsync("Impossibile espellere l'utente - DSharpPlus.Exceptions.BadRequestException");
+            }
+            catch (ServerErrorException)
+            {
+                await command.RespondAsync("Errore del Server - DSharpPlus.Exceptions.ServerErrorException");
+            }
+        }
+    }
+}
