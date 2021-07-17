e785d9d9f868e82ba03ae438d72cfb058591ee9d
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-17 18:57:06 +0200

    Create ClearComando.cs

---
+++ Comandi/Moderazione/ClearComando.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Exceptions;
+
+namespace KheetoNetworkBot.Comandi.Moderazione
+{
+    public class ClearComando : BaseCommandModule
+    {
+        [Command("clear")]
+        [Description("Cancella il determinato numero di messaggi.")]
+        [RequirePermissions(DSharpPlus.Permissions.ManageMessages)]
+        public async Task Comando(CommandContext command, [Description("Numero di messaggi da cancellare.")] int Numero)
+        {
+            if (Numero <= 0)
+            {
+                await command.RespondAsync("Non posso eliminare 0 o meno messaggi!");
+                return;
+            }
+
+            try
+            {
+                await command.Channel.DeleteMessagesAsync(command.Channel.GetMessagesAsync(Numero).Result);
+            }
+            catch (BadRequestException)
+            {
+                await command.Message.RespondAsync("Non posso eliminare messaggi più vecchi di 14 giorni per le limitazioni di Discord.");
+            }
+
+        }
+    }
+}
