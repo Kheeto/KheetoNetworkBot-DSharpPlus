e56f027e0ae3e32e5ef5905d4da2a972cca04234
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-17 20:22:56 +0200

    Update CancellaComando.cs

--- Comandi/Moderazione/CancellaComando.cs
+++ Comandi/Moderazione/CancellaComando.cs
@@ -10,7 +10,7 @@ namespace KheetoNetworkBot.Comandi.Moderazione
 {
     public class CancellaComando : BaseCommandModule
     {
-        [Command("cancella")]
+        [Command("Cancella")]
         [Description("Cancella il determinato numero di messaggi.")]
         [RequirePermissions(DSharpPlus.Permissions.ManageMessages)]
         public async Task Comando(CommandContext command, [Description("Numero di messaggi da cancellare.")] int Numero)
