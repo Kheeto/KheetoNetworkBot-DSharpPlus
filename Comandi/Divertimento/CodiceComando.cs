137a009dde200e4c72399f6f5f664446462fed7b
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-26 21:46:10 +0200

    Create CodiceComando.cs

---
+++ Comandi/Divertimento/CodiceComando.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace KheetoNetworkBot.Comandi.Divertimento
+{
+    public class CodiceComando : BaseCommandModule
+    {
+
+        [Command("codice")]
+        [Description("Minigame - Il primo che scrive il codice generato dal bot vince.")]
+        public async Task Comando(CommandContext command)
+        {
+            var interactivity = command.Client.GetInteractivity();
+
+            var codebytes = new byte[8];
+            using (var rng = RandomNumberGenerator.Create())
+                rng.GetBytes(codebytes);
+
+            var code = BitConverter.ToString(codebytes).ToLower().Replace("-", "");
+
+            await command.RespondAsync($"Il primo a scrivere questo codice vince: `{code}`");
+
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Content.Contains(code), TimeSpan.FromSeconds(60));
+            if (!msg.TimedOut)
+            {
+                await msg.Result.RespondAsync($"{msg.Result.Author.Mention} ha vinto!");
+            }
+            else
+            {
+                await command.RespondAsync("Nessuno ha scritto il codice in tempo lol");
+            }
+        }
+
+    }
+}
+
