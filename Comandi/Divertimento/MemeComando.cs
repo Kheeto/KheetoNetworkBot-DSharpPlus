c06985adfeb7a6ec344294cfef0185dea90e6b71
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-27 19:21:53 +0200

    Update MemeComando.cs

--- Comandi/Divertimento/MemeComando.cs
+++ Comandi/Divertimento/MemeComando.cs
@@ -1,6 +1,7 @@
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using KheetoNetworkBot.Meme;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,30 +18,61 @@ namespace KheetoNetworkBot.Comandi.Divertimento
 
             await command.TriggerTypingAsync();
 
-            DiscordEmbedBuilder embed = new DiscordEmbedBuilder
+            int random = new Random().Next(1, 16);
+
+            switch(random)
             {
-                Title = "test xd",
-                Color = new DiscordColor("#CD0000"),
-                ImageUrl = "C:/Users/Utente/source/repos/KheetoNetworkBot/KheetoNetworkBot/Comandi/Divertimento/FotoMeme/Meme1.jfif"
-            };
+                case 1:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrvcm.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 2:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrz6o.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 3:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrzcd.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 4:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrzff.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 5:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrzkf.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 6:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrzmi.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 7:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrzo2.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 8:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrzrd.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 9:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hrzup.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 10:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hs05a.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 11:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hs07j.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 12:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hs09x.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 13:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hs0ek.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 14:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hs0gb.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 15:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hs0ln.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+                case 16:
+                    await new MemeMaker("Meme!", "https://i.imgflip.com/5hs0ph.jpg", command).RunAsync().ConfigureAwait(false);
+                    return;
+            }
 
-            await command.RespondAsync(embed);
 
-
-        }
-
-        public async Task MemeAsync(string fileName, string embedTitle, CommandContext command)
-        {
-            await command.TriggerTypingAsync();
-
-            DiscordEmbedBuilder embed = new DiscordEmbedBuilder
-            {
-                Title = embedTitle,
-                Color = new DiscordColor("#CD0000"),
-                ImageUrl = "./FotoMeme/" + fileName
-            };
-
-            await command.RespondAsync(embed);
         }
 
     }
