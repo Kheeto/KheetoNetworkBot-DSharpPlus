d8abdcee8e07ef785af4c629f3e5f5912e6bad10
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-27 15:56:48 +0200

    Create Operations.cs

---
+++ Utils/Operations.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KheetoNetworkBot.Utils
+{
+    public enum MathOperation
+    {
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        PERCENT
+    }
+}
