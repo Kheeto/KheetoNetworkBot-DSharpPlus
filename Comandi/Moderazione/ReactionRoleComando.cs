60183c89a6762509256f2917d678150031af872d
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-29 13:35:53 +0200

    Create ReactionRoleComando.cs

---
+++ Comandi/Moderazione/ReactionRoleComando.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
+using KheetoNetworkBot.LIB;
+
+namespace KheetoNetworkBot.Comandi.Moderazione
+{
+    public class ReactionRoleComando : BaseCommandModule
+    {
+        [Command("ReactionRole")]
+        [Description("Crea un messaggio con un reaction role.")]
+        [RequirePermissions(DSharpPlus.Permissions.ManageGuild)]
+        public async Task Comando(CommandContext command, string title, string description, string color, DiscordEmoji emoji, DiscordRole role)
+        {
+            await new SingleReactionRole(command, title, description, color, emoji, role).RunAsync().ConfigureAwait(false);
+        }
+    }
+}
